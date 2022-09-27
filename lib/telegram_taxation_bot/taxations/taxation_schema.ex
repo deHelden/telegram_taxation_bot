685cd60b6e95ab7b15f04023883109320468f03f
Ivan Zinkevich <kevichivan@gmail.com>
2022-09-27 15:59:18 +0400

    add field to taxation_schema

--- lib/telegram_taxation_bot/taxations/taxation_schema.ex
+++ lib/telegram_taxation_bot/taxations/taxation_schema.ex
@@ -2,6 +2,7 @@ defmodule TelegramTaxationBot.Taxations.TaxationSchema do
   use Ecto.Schema
 
   schema "taxations" do
+    field(:user_id, :integer)
     field(:date, :date)
     field(:monthly_amount, :decimal)
     field(:yearly_amount, :decimal)
