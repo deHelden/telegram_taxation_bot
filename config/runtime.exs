48c7c07dde141895d9693ebcbf7864585d44dbdd
Ivan Zinkevich <kevichivan@gmail.com>
2022-11-20 22:49:27 +0400

    update configuration files for deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,26 +1,23 @@
+# In this file, we load production configuration and secrets
+# from environment variables. You can also hardcode secrets,
+# although such is generally not recommended and you have to
+# remember to add this file to your .gitignore.
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
-
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/telegram_taxation_bot start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :telegram_taxation_bot, TelegramTaxationBotWeb.Endpoint, server: true
-end
-
 if config_env() == :prod do
+  defmodule Helpers do
+    def get_env(name) do
+      case System.get_env(name) do
+        nil -> raise "Environment variable #{name} is not set!"
+        val -> val
+      end
+    end
+  end
+
+  # Ensure required env variable are available
+  Helpers.get_env("DASHBOARD_USER")
+  Helpers.get_env("DASHBOARD_PASSWORD")
+
   database_url =
     System.get_env("DATABASE_URL") ||
       raise """
@@ -36,11 +33,6 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -48,36 +40,29 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
   config :telegram_taxation_bot, TelegramTaxationBotWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+    force_ssl: [hsts: true],
+    https: [
+      port: 443,
+      compress: true,
+      cipher_suite: :strong,
+      keyfile: Helpers.get_env("CO2_OFFSET_SSL_KEY_PATH"),
+      cacertfile: Helpers.get_env("CO2_OFFSET_SSL_CACERT_PATH"),
+      certfile: Helpers.get_env("CO2_OFFSET_SSL_CERT_PATH"),
+      transport_options: [socket_opts: [:inet6]]
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    live_view: [signing_salt: Helpers.get_env("telegram_taxation_bot_LIVE_VIEW_SALT")]
 
-  # ## Configuring the mailer
+  # ## Using releases (Elixir v1.9+)
   #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
+  # If you are doing OTP releases, you need to instruct Phoenix
+  # to start each relevant endpoint:
   #
-  #     config :telegram_taxation_bot, TelegramTaxationBot.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
+  config :telegram_taxation_bot, TelegramTaxationBotWeb.Endpoint, server: true
+  config :nadia, token: Helpers.get_env("TELEGRAM_TOKEN")
+
   #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+  # Then you can assemble a release by calling `mix release`.
+  # See `mix help release` for more information.
 end
